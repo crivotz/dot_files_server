cd7a1b3c8d372681e122b11d82485c633458f8c7
Mauro Locatelli <mauro.locatelli@aretes.net>
2019-01-16 16:44:01 +0100

    Symbolic links script and fixed name for TMUX session

---
+++ create_symbolic_links.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+ln -sf ~/.dot_files_server/gemrc ~/.gemrc
+ln -sf ~/.dot_files_server/gitconfig ~/.gitconfig
+ln -sf ~/.dot_files_server/irbrc ~/.irbrc
+ln -sf ~/.dot_files_server/tmux.conf ~/.tmux.conf
+ln -sf ~/.dot_files_server/zsh_aliases ~/.zsh_aliases
+ln -sf ~/.dot_files_server/zlogin ~/.zlogin
+ln -sf ~/.dot_files_server/zpreztorc ~/.zpreztorc
+ln -sf ~/.dot_files_server/zprofile ~/.zprofile
+ln -sf ~/.dot_files_server/zshenv ~/.zshenv
+ln -sf ~/.dot_files_server/zshrc ~/.zshrc
