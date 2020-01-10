dd90ccc2144790a6b4c0b4ea8d94cb72e9c92406
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2020-01-10 17:02:45 +0100

    Update to zplugin and clean

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -5,8 +5,5 @@ ln -sf ~/.dot_files_server/gitconfig ~/.gitconfig
 ln -sf ~/.dot_files_server/irbrc ~/.irbrc
 ln -sf ~/.dot_files_server/tmux.conf ~/.tmux.conf
 ln -sf ~/.dot_files_server/zsh_aliases ~/.zsh_aliases
-ln -sf ~/.dot_files_server/zlogin ~/.zlogin
-ln -sf ~/.dot_files_server/zpreztorc ~/.zpreztorc
-ln -sf ~/.dot_files_server/zprofile ~/.zprofile
-ln -sf ~/.dot_files_server/zshenv ~/.zshenv
-ln -sf ~/.dot_files_server/zshrc ~/.zshrc
+ln -sf ~/.dot_files_server/zplugin/zshrc ~/.zshrc
+ln -sf ~/.dot_files_server/ranger ~/.config/ranger
