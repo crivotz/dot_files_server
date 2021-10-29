3fd56387f642da8f419068914d3d1d5f3544fb22
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2021-10-29 11:56:54 +0200

    Fix some path and clean

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -5,7 +5,7 @@ ln -sf ~/.dot_files_server/gitconfig ~/.gitconfig
 ln -sf ~/.dot_files_server/irbrc ~/.irbrc
 ln -sf ~/.dot_files_server/tmux.conf ~/.tmux.conf
 ln -sf ~/.dot_files_server/zsh_aliases ~/.zsh_aliases
-ln -sf ~/.dot_files_server/zplugin/zshrc ~/.zshrc
+ln -sf ~/.dot_files_server/zinit/zshrc ~/.zshrc
 ln -sf ~/.dot_files_server/ranger ~/.config/ranger
 ln -sf ~/.dot_files_server/p10k.zsh ~/.p10k.zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
