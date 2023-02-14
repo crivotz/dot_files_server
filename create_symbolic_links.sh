d44d0e62648df04236232a3624ec0a3cda51b5ef
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2023-02-14 10:32:50 +0100

    Updated to last dot_files

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -9,4 +9,5 @@ ln -sf ~/.dot_files_server/zinit/zshrc ~/.zshrc
 ln -sf ~/.dot_files_server/ranger ~/.config/ranger
 ln -sf ~/.dot_files_server/p10k.zsh ~/.p10k.zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git -C ~/.tmux/plugins/tpm pull || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
